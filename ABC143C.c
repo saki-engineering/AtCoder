35676e1d1c1a2e5adc4bcfca7890dbfc6c19c8aa
Hsakihara <sakiharu31415@yahoo.co.jp>
2019-10-20 21:32:54 +0900

    ABC143のACコード(string_input未使用)

---
+++ ABC143C.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define NEW(p,n){p=malloc((n)*sizeof(p[0]));if(p==NULL){printf("not enough memory\n");exit(1);};}
+//pの型の変数n個の要素分のメモリを確保し、そのアドレスをpに代入するマクロ
+
+//char型のポインタ(=配列と意味は同じ)をString型と定義([0]から数える)
+typedef char* String;
+
+
+int main(void){
+    int N;
+    scanf("%d",&N);
+
+    String S;
+    NEW(S,N+1);
+    scanf("%s", S);
+
+    int ans=1;
+    for(int i=1;i<N;i++){
+        if(S[i]!=S[i-1]){
+            ans++;
+        }
+    }
+    printf("%d\n",ans);
+
+    return 0;
+}
