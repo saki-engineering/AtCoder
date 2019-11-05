ea70faabe875c0c6bff6f85b950bca47f6d17a89
Hsakihara <sakiharu31415@yahoo.co.jp>
2019-11-05 18:25:20 +0900

    ABC114CのよりスマートなACコード

--- ABC114C.c
+++ ABC114C.c
@@ -9,14 +9,19 @@
 long ans=0;
 
 
-int count_753(long s,long N,int a, int b, int c){
-    if(s>N) return ans;
+int count_753(long key,long N,int f3, int f5, int f7){
+    int ans=0;
+    //停止判定(key>N)を、再帰関数の中に組み込んでしまうと楽
+    if(key>N) return ans;
 
-    if(a+b+c==3) ans++;
+    //f3=1 and f5=1 and f7=1を、and文で書くんじゃなくて、ただの足し算で書くとすごくスマートになる
+    if(f3+f5+f7==3) ans++;
 
-    count_753((s*10+3),N,1,b,c);
-    count_753((s*10+5),N,a,1,c);
-    count_753((s*10+7),N,a,b,1);
+    ans+=count_753((key*10+3),N,1,f5,f7);
+    ans+=count_753((key*10+5),N,f3,1,f7);
+    ans+=count_753((key*10+7),N,f3,f5,1);
+
+    return ans;
 }
 
 int main(void){
