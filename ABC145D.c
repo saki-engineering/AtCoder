fefcfbbc324d869d5bd2eb4365c594e2fabd8fe6
Hsakihara <sakiharu31415@yahoo.co.jp>
2019-11-20 00:38:57 +0900

    逆元を求める関数(modinv)を変更

--- ABC145D.c
+++ ABC145D.c
@@ -6,24 +6,22 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define SWAP(type, x, y) do { type tmp = x; x = y; y = tmp; } while (0)
 
-#define MOD 1000000007
-
-void swap(long *xp, long *yp) {
-  long temp = *xp; 
-  *xp = *yp; 
-  *yp = temp; 
-} 
+#define MOD 1000000007 
  
-long modinv(long a) {
-  long b = MOD, u = 1, v = 0;
+//a*x + MOD*y = 1の整数解を求める。その中で最小の正数xがaの逆元となる。
+//解き方は、ユークリッドの互除法
+//参考：http://arc360.info/algo/privatekey.html
+long modinv(long a, long m) {
+  long b = m, u = 1, v = 0;
   while (b) {
     long t = a / b;
-    a -= t * b; swap(&a, &b);
-    u -= t * v; swap(&u, &v);
+    a -= t * b; SWAP(long,a, b);
+    u -= t * v; SWAP(long,u, v);
   }
-  u %= MOD;
-  if (u < 0) u += MOD;
+  u %= m;
+  if (u < 0) u += m;
   return u;
 }
 
@@ -32,7 +30,7 @@ long combination(int n, int m){
     long ans=1;
 
     for(int i=n;i>(n-m);i--) ans=(ans*i)%MOD;
-    for(int i=2;i<=m;i++) ans=(ans*modinv(i))%MOD;
+    for(int i=2;i<=m;i++) ans=(ans*modinv(i,MOD))%MOD;
 
     return ans;
 }
