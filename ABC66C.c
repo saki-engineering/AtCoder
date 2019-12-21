d527e07838f875f83f076596b8ef1897cb767018
Hsakihara <sakiharu31415@yahoo.co.jp>
2019-12-21 14:29:40 +0900

    ACコード

---
+++ ABC66C.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define NEW(p,n){p=malloc((n)*sizeof(p[0]));if(p==NULL){printf("not enough memory\n");exit(1);};}
+//pの型の変数n個の要素分のメモリを確保し、そのアドレスをpに代入するマクロ
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define SWAP(type, x, y) do { type tmp = x; x = y; y = tmp; } while (0)
+
+#define MOD 1000000007 
+
+int main(void){
+    int N;
+    scanf("%d",&N);
+
+    //A[0]~A[N-1]に格納する
+    int* A;
+    NEW(A,N);
+    for(int i=0;i<N;i++){
+        scanf("%d",&A[i]);
+    }
+
+    if(N%2==0){
+        for(int i=(N/2)-1;i>=0;i--) printf("%d ",A[i*2+1]);
+        for(int i=0;i<N/2;i++) printf("%d ",A[i*2]);
+    }
+    else{
+        for(int i=(N/2);i>=0;i--) printf("%d ",A[i*2]);
+        for(int i=0;i<N/2;i++) printf("%d ",A[i*2+1]);
+    }
+    printf("\n");
+
+    return 0;
+}
