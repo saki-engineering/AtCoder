b3332fd5f946a533eb87d7ccb8431eca33040c83
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-01-01 17:16:33 +0900

    ACコード

---
+++ ABC53C.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+#define NEW(p,n){p=malloc((n)*sizeof(p[0]));if(p==NULL){printf("not enough memory\n");exit(1);};}
+//pの型の変数n個の要素分のメモリを確保し、そのアドレスをpに代入するマクロ
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define SWAP(type, x, y) do { type tmp = x; x = y; y = tmp; } while (0)
+
+#define MOD 1000000007 
+
+int main(void){
+    long x;
+    scanf("%ld",&x);
+
+    long ans=(x/11)*2;
+    if(x%11==0) ans+=0;
+    else if(x%11<=6) ans+=1;
+    else ans+=2;
+
+    printf("%ld\n",ans);
+
+    return 0;
+}
---
+++ ABC53D.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+#define NEW(p,n){p=malloc((n)*sizeof(p[0]));if(p==NULL){printf("not enough memory\n");exit(1);};}
+//pの型の変数n個の要素分のメモリを確保し、そのアドレスをpに代入するマクロ
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define SWAP(type, x, y) do { type tmp = x; x = y; y = tmp; } while (0)
+
+#define MOD 1000000007 
+
+//昇順
+int asc(const void* a, const void* b){
+    return *(int*)a-*(int*)b;
+}
+
+int main(void){
+    int N;
+    scanf("%d",&N);
+
+    //A[0]~A[N-1]に格納する
+    int* A;
+    NEW(A,N);
+    for(int i=0;i<N;i++){
+        scanf("%d",&A[i]);
+    }
+    qsort(A,N,sizeof(int),asc);
+
+    int ans=1;
+    for(int i=1;i<N;i++){
+        if(A[i]!=A[i-1]) ans++;
+    }
+    if(ans%2==0) ans--;
+    printf("%d\n",ans);
+
+    return 0;
+}
